cc69aa1f43c4171291ca6ffbe7b24b8b633b24ae
Matheus Ferreira <matheusjuan.f@outlook.com>
2025-04-30 11:44:54 -0300

    feat: add android project demo

---
+++ CoreBridgeAndroid/app/src/main/java/com/matheusjuan/corebridge/android/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.matheusjuan.corebridge.android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.matheusjuan.corebridge.HelloWorld
+import com.matheusjuan.corebridge.android.ui.theme.CoreBridgeAndroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CoreBridgeAndroidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        text = HelloWorld().hello("Matheus"),
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(text: String, modifier: Modifier = Modifier) {
+    Text(
+        text = text,
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CoreBridgeAndroidTheme {
+        Greeting(HelloWorld().hello("Android"))
+    }
+}
