049facea0995a904dff3a64789256c353fba74ee
Matheus Ferreira <matheusjuanf.f@outlook.com>
2025-04-30 10:25:24 -0300

    feat: add lib

---
+++ CoreBridgeLib/corebridge/src/main/java/com/matheusjuan/corebridge/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.matheusjuan.corebridge;
+
+public class HelloWorld {
+
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+    public String hello(String name) {
+        return "Hello " + name + "!";
+    }
+}
