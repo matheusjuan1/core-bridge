b7bdef178fa50308f3329bee4267dca584ca63d0
Matheus Ferreira <matheusjuan.f@outlook.com>
2025-05-10 16:57:10 -0300

    feat: add react on script

--- build-corebridge.bat
+++ build-corebridge.bat
@@ -10,7 +10,8 @@ cd ..
 set AAR_PATH=CoreBridgeLib\corebridge\build\outputs\aar\corebridge-debug.aar
 
 set DEST_ANDROID=CoreBridgeAndroid\app\libs
-set DEST_FLUTTER=CoreBridgeFlutter\android\libs
+set DEST_FLUTTER=CoreBridgeFlutter\plugin\core_bridge_plugin\android\libs
+set DEST_REACT=CoreBridgeReact\android\libs
 
 echo 📦 Copiando %AAR_PATH% para %DEST_ANDROID%...
 if not exist %DEST_ANDROID% mkdir %DEST_ANDROID%
@@ -19,5 +20,9 @@ copy /Y %AAR_PATH% %DEST_ANDROID%\core-bridge.aar
 echo 📦 Copiando %AAR_PATH% para %DEST_FLUTTER%...
 if not exist %DEST_FLUTTER% mkdir %DEST_FLUTTER%
 copy /Y %AAR_PATH% %DEST_FLUTTER%\core-bridge.aar
+
+echo 📦 Copiando %AAR_PATH% para %DEST_REACT%...
+if not exist %DEST_REACT% mkdir %DEST_REACT%
+copy /Y %AAR_PATH% %DEST_REACT%\core-bridge.aar
 
 echo ✅ Build e cópias finalizadas com sucesso.
